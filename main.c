827f554f1f0435513f70a21739fe1aa2f6d57137
CupOJava <37489840+Anujpatel2@users.noreply.github.com>
2020-03-06 00:52:15 -0500

    Added the Read from file for solved sudoku

--- main.c
+++ main.c
@@ -12,17 +12,21 @@ typedef struct{
     int column;
 }parameters;
 
-    int sudoku [9][9] = {
-        5, 3, 4, 6, 7, 8, 9, 1, 2,
-        6, 7, 2, 1, 9, 5, 3, 4, 8,
-        1, 9, 8, 3, 4, 2, 5, 6, 7,
-        8, 5, 9, 7, 6, 1, 4, 2, 3,
-        4, 2, 6, 8, 5, 3, 7, 9, 1,
-        7, 1, 3, 9, 2, 4, 8, 5, 6,
-        9, 6, 1, 5, 3, 7, 2, 8, 4,
-        2, 8, 7, 4, 1, 9, 6, 3, 5,
-        3, 4, 5, 2, 8, 6, 1, 7, 9
-    };
+    FILE* file = fopen("Sudoku_puzzle.txt", "r");
+	int num;
+	int row = 0;
+	int col = 0;
+	int sudoku[9][9];
+
+	while (row < 9) {
+		while (col < 9) {
+			fscanf(file, "%d", &num);
+			sudoku[col][row] = num;
+			col++;
+		}
+		col = 0;
+		row++;
+	}
     
     void *checkRow(parameters *rowParam){
         int r = rowParam->row;
